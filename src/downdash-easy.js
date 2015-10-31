21599cf4c0f587536f4679faa1f93782c162bc26
Doug Shamoo <doug.shamoo@gmail.com>
2015-10-31 00:56:18 -0700

    implement all downdash-easy functions

--- src/downdash-easy.js
+++ src/downdash-easy.js
@@ -18,7 +18,16 @@
     >>> 2
 */
   _.each = function(collection, iterator){
-
+    if (!collection) return; // handle null object
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; ++i) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
 /*
@@ -29,7 +38,10 @@
     >>> 1
 */
   _.indexOf = function(array, value){
-
+    for (var i = 0; i < array.length; ++i) {
+      if (array[i] === value) return i; 
+    }
+    return -1;
   };
 
 /*
@@ -44,7 +56,11 @@
     >>> [3, 6]
 */
   _.map = function(collection, iterator){
-
+    var results = [];
+    _.each(collection, function(value) {
+      results.push(iterator(value));
+    });
+    return results;
   };
   
 /*
@@ -57,7 +73,11 @@
     >>> 3
 */
   _.reduce = function(collection, iterator, accumulator){
-
+    _.each(collection, function(value) {
+      if (accumulator === undefined) accumulator = value;
+      else accumulator = iterator(accumulator, value);
+    });
+    return accumulator;
   };
 
 /*
@@ -70,7 +90,12 @@
     >>> [4, 6]
 */
   _.filter = function(collection, predicate){
-
+    return _.reduce(collection, function(results, value) {
+      if (predicate(value)) {
+        results.push(value);
+      }
+      return results;
+    }, []);
   };
 
 /*
@@ -81,7 +106,13 @@
     >>> ['a', 'c']
 */
   _.at = function(collection){
-
+    var indices = Array.prototype.slice.call(arguments, 1);
+    if (!indices) return [];
+    indices = Array.isArray(indices[0]) ? indices[0] : indices;
+    return _.reduce(indices, function(results, index) {
+      results.push(collection[index]);
+      return results;
+    }, []);
   };
 
 /*
@@ -94,7 +125,10 @@
     >>> false
 */
   _.every = function(collection, predicate){
-
+    return _.reduce(collection, function(isGood, item) {
+      if (!isGood) return false;
+      return predicate(item);
+    }, true);
   };
 
 /*
@@ -105,7 +139,10 @@
     >>> true
 */
   _.includes = function(collection, target){
-
+    return _.reduce(collection, function(found, value) {
+      if (found) return found;
+      return (value === target);
+    }, false)
   };
 
 /*
@@ -118,7 +155,11 @@
     >>> [[1, 3], [2]]
 */
   _.partition = function(collection, predicate){
-
+    return _.reduce(collection, function(partitions, value) {
+      if (predicate(value)) partitions[0].push(value);
+      else partitions[1].push(value);
+      return partitions;
+    }, [[], []]);
   };
 
 }());
